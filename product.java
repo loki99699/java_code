0094450e1df2da2e6d0ef5be3860863a03b8df26
loki99699 <lokeshpoojary367@gmail.com>
2025-01-29 09:38:38 +0530

    Update product.java

--- product.java
+++ product.java
@@ -2,9 +2,10 @@ class product
 {
   public static void main(String args[])
   {
-    int a=5;
-    int b=2;
+    int a,b,product;
+    a=5;
+    b=2;
     product=a*b;
-    System.out.println("product of two numbers is", +product);
+    System.out.println("product of two numbers is" +product);
   }
 }
